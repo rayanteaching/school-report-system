8969d30d3b4f27150a33b872c53770993f57e4fc
iliyajpn <iliyajahan2023@gmail.com>
2026-06-15 20:56:50 +0330

    implement add student function

---
+++ students.py
@@ -0,0 +1,29 @@
+def add_student(data , username , password):
+    """
+    Add a new student to the data dictianory.
+
+    Parameters:
+        data : dictionary of all students
+        username : new student's username
+        password : new student's password
+
+    Returns:
+        updated data dictionary
+    """
+
+    if username in data:
+        return data
+
+    data[username] = {
+        "password" : password,
+        "grades" : {}
+    }
+
+    return data
+
+if __name__ == "__main__":
+    students_data = {}
+
+    students_data = add_student(students_data , "ali" , "1111")
+    students_data = add_student(students_data , "sara" , "2222")
+    print(students_data)
